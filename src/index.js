a1cbc31b1314e9dcab8d9740e3481d9503847515
Nikita <kitoos93@gmail.com>
2019-11-06 16:47:35 +0700

    Add comments and remove unnecessary code

--- src/index.js
+++ src/index.js
@@ -1,101 +1,106 @@
+//Приложение предлагает пользователю ввести две даты: начало периода и конец периода
+//После ввода дат и нажатия кнопки эти значения записываются в таблицу под формой
+//В отдельном столбце таблицы рассчитывается кол-во календарных дней между введенными датами
+
 import React from "react";
 import ReactDOM from "react-dom";
 import './index.css';
 import moment from "moment";
 import DatePicker from "react-datepicker";
-
 import "react-datepicker/dist/react-datepicker.css";
 
 class Dates extends React.Component {
     constructor(props) {
         super(props);
         
+        //Будем хранить массив введенных дат (для таблицы) и текущие даты в окнах ввода
         this.state = {
             items: [],
             startDate: new Date('2019-10-06T00:00:00'),
             endDate: new Date('2019-11-06T00:00:00'),
         };
         
+        //Привязка обработчиков двух полей и кнопки
         this.handleChangeStart = this.handleChangeStart.bind(this);
         this.handleChangeEnd = this.handleChangeEnd.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    //При изменении даты в поле значение записывается в state
     handleChangeStart(date) {
         this.setState({
             startDate: date
         });
-        console.log('Изменена дата начала: ' + date);
     }
 
+    //При изменении даты в поле значение записывается в state
     handleChangeEnd(date) {
         this.setState({
             endDate: date
         });
-        console.log('Изменена дата конца: ' + date);
     }
 
+    //Обработка кнопки
     handleSubmit(e) {
-        e.preventDefault();
-          
+        e.preventDefault(); //?
+        
+        //Проверяем, заполнены ли обе даты. Если нет - заканчиваем
         if (!this.state.startDate || !this.state.endDate) {
-            console.log('Не все даты заполнены');
             return;
         }
-    
+        
+        //Даты заполнены, значит создаем объект, где сохраним нужные данные
         const newItem = {
-            startDate: this.state.startDate,
-            endDate: this.state.endDate,
-            diff: this.calculateDaysLeft(this.state.startDate, this.state.endDate),
-            id: Date.now()
+            startDate: this.state.startDate, //Первая дата
+            endDate: this.state.endDate, //Вторая дата
+            diff: this.calculateDaysLeft(this.state.startDate, this.state.endDate), //Кол-во дней между датами
+            id: Date.now() //Текущая дата для идентификации
         };
-        console.log('Создана новая сущность');
 
+        //Записываем объект в массив в state
         this.setState(state => ({
             items: state.items.concat(newItem)
         }));
-        console.log('Новая сущность добавлена в список');
     }
 
-  calculateDaysLeft(startDate, endDate) {
-    if (!moment.isMoment(startDate)) startDate = moment(startDate);
-    if (!moment.isMoment(endDate)) endDate = moment(endDate);
+    //Функция для расчета кол-ва дней между двумя датами
+    calculateDaysLeft(startDate, endDate) {
+        if (!moment.isMoment(startDate)) startDate = moment(startDate);
+        if (!moment.isMoment(endDate)) endDate = moment(endDate);
 
-    return endDate.diff(startDate, "days");
-    //return endDate -
-  }
+        return endDate.diff(startDate, "days");
+    }
 
-  render() {
-    const { startDate, endDate } = this.state;
-
-    const daysLeft = this.calculateDaysLeft(startDate, endDate);
-    return (
-        <div>
-            <h1>Разница между двумя датами</h1>
-            <form onSubmit={this.handleSubmit}>
-                    <label>Начало периода:</label><br/>
-                    <DatePicker
-                    dateFormat="dd.MM.yyyy"
-                    selected={this.state.startDate}
-                    onChange={this.handleChangeStart}
-                    /><br/>
-                    <label>Конец периода:</label><br/>
-                    <DatePicker
-                    dateFormat="dd.MM.yyyy"
-                    selected={this.state.endDate}
-                    onChange={this.handleChangeEnd}
-                    /><br/><br/><br/>
-                    <button type="submit" class="pure-button pure-button-primary">
-                        Добавить
-                    </button><br/>
-            </form>
-            <br/>
-            <DatesList items={this.state.items} />
-        </div>
-    );
-  }
+    //Рендерим страничку
+    render() {
+        return (
+            <div>
+                <h1>Разница между двумя датами</h1>
+                <form onSubmit={this.handleSubmit}>
+                        <label>Начало периода:</label><br/>
+                        <DatePicker
+                        dateFormat="dd.MM.yyyy"
+                        selected={this.state.startDate}
+                        onChange={this.handleChangeStart}
+                        /><br/>
+                        <label>Конец периода:</label><br/>
+                        <DatePicker
+                        dateFormat="dd.MM.yyyy"
+                        selected={this.state.endDate}
+                        onChange={this.handleChangeEnd}
+                        /><br/><br/><br/>
+                        <button type="submit">
+                            Добавить
+                        </button><br/>
+                </form>
+                <br/>
+                <DatesList items={this.state.items} />
+            </div>
+        );
+    }
 }
 
+//Вывод списка введенных дат в таблицу
 class DatesList extends React.Component {
     render() {
         console.log('Рендерим список');
